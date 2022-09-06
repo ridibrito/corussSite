7790c4cf3f6928ede5fb2d8b3b431eacb7be011b
ridibrito <devoldbsb@gmail.com>
2022-09-06 07:08:12 -0300

    type

--- components/LinkMenu2.tsx
+++ components/LinkMenu2.tsx
@@ -15,7 +15,7 @@ const LinkMenu2 = ({ children, href }: Props) => {
     const selected = pathname === href
 
     return (
-        
+         //@ts-ignore
         <Link href={href}>
             <a className={selected
                 ? "flex dark:text-gray-300  items-center pt-2 pl-2 pb-2 text-sky-600 hover:cursor-pointer  rounded-md m-2"
