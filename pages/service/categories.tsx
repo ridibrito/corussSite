27373329b0cd43dc682b893c076d4be8f304c692
ridibrito <devoldbsb@gmail.com>
2022-10-25 15:52:45 -0300

    125

--- pages/service/categories.tsx
+++
@@ -1,9 +0,0 @@
-import  {  Category  }  from  '../../types/Category' ;
-
-
-export const categories: Category = {
-    food:{ title: 'Alimentação', expense: true},
-    rent: {title: 'Aluguel' , expense: true},
-    salary:{title: 'Salário' , expense: false}
-}
-export default categories
