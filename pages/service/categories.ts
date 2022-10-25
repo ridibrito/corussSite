b634888e003114ec19e6c4fc39db1228b66eb55a
ridibrito <devoldbsb@gmail.com>
2022-10-25 16:02:13 -0300

    122

---
+++ pages/service/categories.ts
@@ -0,0 +1,7 @@
+import { Category } from '../../types/Category';
+
+export const categories: Category = {
+    food: { title: 'Alimentação', expense: true },
+    rent: { title: 'Aluguel', expense: true },
+    salary: { title: 'Salário', expense: false }
+}
