1842940bacf159dbcbc44f0fcb985a01e2194b86
ricardo <linux@pop-os.localdomain>
2022-11-17 18:18:46 -0300

    snov

--- components/site/Diferencas.tsx
+++ components/site/Diferencas.tsx
@@ -28,23 +28,7 @@ export default function Diferecas() {
                   Substitui todas as soluções atuais
                 </span>
               </li>
-              <li className="flex items-center space-x-3 mb-2">
-                <svg
-                  className="flex-shrink-0 w-5 h-5 text-green-500 dark:text-green-400"
-                  fill="currentColor"
-                  viewBox="0 0 20 20"
-                  xmlns="http://www.w3.org/2000/svg"
-                >
-                  <path
-                    fillRule="evenodd"
-                    d="M16.707 5.293a1 1 0 010 1.414l-8 8a1 1 0 01-1.414 0l-4-4a1 1 0 011.414-1.414L8 12.586l7.293-7.293a1 1 0 011.414 0z"
-                    clipRule="evenodd"
-                  ></path>
-                </svg>
-                <span className="font-bold">
-                  Grátis para sempre para 1 usuário
-                </span>
-              </li>
+         
               <li className="flex items-center space-x-3 mb-2">
                 <svg
                   className="flex-shrink-0 w-5 h-5 text-green-500 dark:text-green-400"
@@ -75,7 +59,7 @@ export default function Diferecas() {
                     clipRule="evenodd"
                   ></path>
                 </svg>
-                <span className="font-bold">Simulador gratuito para todos </span>
+                <span className="font-bold">Simulador gratuito para sempre </span>
               </li>
               <li className="flex items-center space-x-3 mb-2">
                 <svg
--- components/site/headerSite.tsx
+++ components/site/headerSite.tsx
@@ -1,30 +1,38 @@
-import Image from "next/image"
-import Link from "next/link"
+import Image from "next/image";
+import Link from "next/link";
 
 export default function HeaderSite() {
   return (
     <>
-      <header className="sm:flex sm:max-w-6xl sm:justify-center sm:mx-auto ">
+      <header className="sm:flex sm:max-w-7xl sm:mx-auto sm:mt-20">
         <div className="sm:mx-auto mx-3 sm:w-1/2 mt-4">
           <h4 className=" text-gray-700 text-lg font-semibold">
             Tudo em um só lugar.
           </h4>
-          <h1 className="text-sky-600 text-4xl pt-2 font-bold md:text-4xl lg:text-6xl">
+          <h1 className="text-sky-600 text-4xl   font-bold md:text-4xl lg:text-6xl">
             coruss 360º
           </h1>
           <h1 className="text-gray-700 text-3xl pt-2 font-bold md:text-4xl lg:text-5xl">
-           Gerencie suas vendas de planos de saúde.
+            Gerencie suas vendas de planos de saúde.
           </h1>
-          <h1 className="text-gray-700 sm:text-lg pt-2 font-semibold">
-             Atendimento, Venda, gerenciamento, Financeiro, relatórios e muito mais...
+          <h1 className="text-gray-700 sm:text-lg pt-2 font-semibold sm:mt-3">
+            Expanda seus negócios e envolva melhor os leads com as ferramentas
+            de vendas e a plataforma de CRM que trazem crescimento.
           </h1>
-          <Link href="/api/auth/signin"><a><button className="bg-sky-600 text-white font-bold py-2 rounded mt-3 w-full text-center items-center md:px-32 lg:px-36 xl:px-full hover:bg-sky-700">
-            Teste grátis
-          </button></a></Link>
-          
+          <div className="mt-3">
+            <Link href="/api/auth/signin">
+              <a>
+                <button className="bg-sky-600 text-white font-bold py-2 rounded mt-3 w-full text-center items-center md:px-32 lg:px-36 xl:px-full hover:bg-sky-700 sm:py-3">
+                  Teste grátis
+                </button>
+              </a>
+            </Link>
+          </div>
         </div>
-        <div className="mt-5 flex justify-center sm:w-2/5"><Image src="/bgimg.png" alt="crm" width="400" height="400"/></div>
+        <div className="mt-5 flex justify-center sm:w-2/5">
+          <Image src="/bgimg.png" alt="crm" width="400" height="400" />
+        </div>
       </header>
     </>
-  )
+  );
 }
